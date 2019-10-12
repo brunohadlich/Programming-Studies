8e479d013d24012094dfacaba3750908c8250a5b
Bruno <brunosimashadlich@gmail.com>
2019-10-12 13:48:17 -0300

    Adding openssl usage

---
+++ c_linux/openssl/main.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10/09/2019 07:46:19 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <openssl/evp.h>
+#include <openssl/ssl.h>
+#include <openssl/rsa.h>
+#include <openssl/x509.h>
+
+int main() {
+	return 0;
+}
