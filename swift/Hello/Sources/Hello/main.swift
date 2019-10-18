78487a9fc16f3c18cacefa8cd9da308e0c3a3533
Bruno Simas Hadlich <bruno.hadlich@spartalabs.com.br>
2019-10-18 14:33:18 -0300

    Adding swift examples and renaming cpp and nodejs files

---
+++ swift/Hello/Sources/Hello/main.swift
@@ -0,0 +1,10 @@
+func sayHello(name: String) {
+	print("Hello, \(name)!")
+}
+
+if CommandLine.arguments.count != 2 {
+	print("Usage: hello NAME")
+} else {
+	let name = CommandLine.arguments[1]
+	sayHello(name: name)
+}
