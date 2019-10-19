da2c3cebe2a018ac2e8f096836f4727a1659d9a5
Bruno <brunosimashadlich@gmail.com>
2019-10-18 21:57:10 -0300

    Adding aes 128 bits key implementation

---
+++ c_linux/criptography/aes/main.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10/18/2019 09:14:41 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+int main(void) {
+	printf("\n%d\n", 0x45);
+	printf("\n%d\n", 0b11);
+	printf("\n%d\n", 1 << 0);
+}
