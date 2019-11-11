136dc95125adce2d395f00b7fa0b90be34d7b812
Bruno <brunosimashadlich@gmail.com>
2019-11-11 20:16:35 -0300

    Added cache and packing examples

--- c_linux/cache/main.c
+++ c_linux/cache/main.c
@@ -3,27 +3,47 @@
 
 void slow() {
         char *v = malloc(100000000);
-        int x, y;
+        int x, y, k = 0;
         for (x = 0; x < 10000; x++) {
                 for (y = x; y < 100000000; y+=10000) {
+			k++;
                         v[y] = v[y];
                 }
         }
+	printf("Iterated: %d times.\n", k);
         free(v);
 }
 
 void fast() {
         char *v = malloc(100000000);
-        int x, y;
+        int x, y, k = 0;
         for (x = 0; x < 100000000; x += 10000) {
                 int until = x + 10000;
                 for (y = x; y < until; y++) {
+			k++;
                         v[y] = v[y];
                 }
         }
+	printf("Iterated: %d times.\n", k);
+        free(v);
+}
+
+void very_fast() {
+        char *v = malloc(100000000);
+        int x, y, k = 0;
+        for (x = 0; x < 100000000; x += 10000) {
+                int until = x + 10000;
+                for (y = x; y < until; y++) {
+			k++;
+                        v[0] = v[0];
+                }
+        }
+	printf("Iterated: %d times.\n", k);
         free(v);
 }
 
 int main() {
-        fast();
+        //slow();
+        //fast();
+	very_fast();
 }
---
+++ c_linux/packing/main2.c
@@ -0,0 +1,52 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/11/2019 07:07:41 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+struct s1 {
+	long double v1;
+	char v2;
+	long double v3;
+	char v4;
+	long double v5;
+	char v6;
+	long double v7;
+	char v8;
+};
+
+struct s2 {
+	long double v1;
+	long double v3;
+	long double v5;
+	long double v7;
+	char v2;
+	char v4;
+	char v6;
+	char v8;
+};
+
+int main() {
+	printf("long double: %lu\n", sizeof(long double));
+	printf("struct s1: %lu\n", sizeof(struct s1));
+	printf("struct s2: %lu\n", sizeof(struct s2));
+	struct s2 *big_array = (struct s2*)malloc(sizeof(struct s2) * 1000000);
+	if (big_array == NULL) {
+		printf("Falhou");
+	}
+	while(1);
+}
